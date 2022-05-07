82fc45816efe33feea12862eea22041ffe25a714
shy realm <gtsbyxx@gmail.com>
2022-05-07 11:44:24 -0400

    created separate method for link scraper

--- main.py
+++ main.py
@@ -3,22 +3,35 @@ from bs4 import BeautifulSoup
 import re
 import praw
 
-query = ' best shampoo' #input goes here
+def link_scraper(t):
+    query = t
 
-search = query.replace(' ', '+')
-results = 10
-url = (f"https://www.google.com/search?q=site:reddit.com+{search}&num={results}")
+    search = query.replace('', '+')
+    results = 10
+    url = (f"https://www.google.com/search?q=site:reddit.com+{search}&num={results}")
 
-requests_results = requests.get(url)
-soup_link = BeautifulSoup(requests_results.content, "html.parser")
-links = soup_link.find_all("a")
+    requests_results = requests.get(url)
+    soup_link = BeautifulSoup(requests_results.content, "html.parser")
+    links = soup_link.find_all("a")
 
-a = []
-for link in links:
-    link_href = link.get('href')
-    if "url?q=" in link_href and not "webcache" in link_href:
-      title = link.find_all('h3')
-      if len(title) > 0:
-          a.append(link.get('href').split("?q=")[1].split("&sa=U")[0])
+    a = []
+    for link in links:
+        link_href = link.get('href')
+        if "url?q=" in link_href and not "webcache" in link_href:
+            title = link.find_all('h3')
+            if len(title) > 0:
+                a.append(link.get('href').split("?q=")[1].split("&sa=U")[0])
+    
+    print(a)
 
-print("Values of a:", a)    
+def reddit_method(links):    
+    reddit = praw.Reddit(client_id ='zkx-1C4UeAGQrvd-UDC92g',
+                    client_secret ='aDpjcO0AUzDpEqfa1zoJvMxQI72Bvg',
+                    user_agent ='redview.com by /u/redview_script',
+                    username ='redview_script',
+                    password ='redview_password')
+    
+    # submission = reddit.submission()
+
+print(link_scraper('best shampoo'))
+
